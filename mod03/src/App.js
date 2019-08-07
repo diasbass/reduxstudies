7122b63fd071fb30e77ebe3e93c3e28b3ec40345
Rodrigo Dias <diasbass@hotmail.com>
2019-08-07 17:08:04 -0300

    New application

--- mod03/src/App.js
+++ mod03/src/App.js
@@ -3,13 +3,14 @@ import "./config/ReactotronConfig";
 import { Provider } from "react-redux";
 
 import store from "./store";
-import TodoList from "./TodoList";
 
-console.tron.log("Testando");
+//console.tron.log("Testando");
+
+import Routes from "./routes";
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <Routes />
   </Provider>
 );
 
