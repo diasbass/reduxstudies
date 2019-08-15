93dc240da7fc94e5852ec3df9397f929da07d126
Rodrigo Dias <diasbass@hotmail.com>
2019-08-15 16:20:33 -0300

    Plugin do saga no reactotron

--- mod03/src/config/ReactotronConfig.js
+++ mod03/src/config/ReactotronConfig.js
@@ -1,9 +1,12 @@
-import Reactotron from 'reactotron-react-js';
-import { reactotronRedux } from 'reactotron-redux';
+import Reactotron from "reactotron-react-js";
+import { reactotronRedux } from "reactotron-redux";
 
-if (process.env.NODE_ENV === 'development') {
+import sagaPlugin from "reactotron-redux-saga";
+
+if (process.env.NODE_ENV === "development") {
   const tron = Reactotron.configure()
-    .use(reactotronRedux())    
+    .use(reactotronRedux())
+    .use(sagaPlugin())
     .connect();
 
   tron.clear();
